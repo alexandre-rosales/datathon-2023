1d8d0e1ee9e456dc7f96ba1b39d19110fa62106a
alexandre-rosales <67807789+alexandre-rosales@users.noreply.github.com>
2023-11-12 12:03:56 +0100

    Actualitzem Outfit_generator.py

--- codis/Outfit_generator.py
+++ codis/Outfit_generator.py
@@ -28,11 +28,11 @@ outfit_final=[peca_actual]
 
 # PART 1: TROBEM UNA PART DE DALT, UNA PART DE BAIX I UNES SABATES
 
-if peca_actual.atributs['des_product_family'] == 'Trousers' or 'Jeans' or 'Skirts' or 'Shorts' or 'Jumpsuit' or 'Leggins and joggers':
+if peca_actual.atributs['des_product_family'] in ('Trousers', 'Jeans', 'Skirts', 'Shorts', 'Jumpsuit', 'Leggins and joggers'):
     need_part_baix = False
     need_part_dalt = True
     need_sabates = True
-elif peca_actual.atributs['des_product_family'] == 'Dresses' or 'Shirt' or 'Swetter' or 'Tops' or 'T-shirt' or 'Bodysuits':
+elif peca_actual.atributs['des_product_family'] in ('Dresses', 'Shirt', 'Swetter', 'Tops', 'T-shirt', 'Bodysuits'):
     need_part_baix = True
     need_part_dalt = False
     need_sabates = True
@@ -49,10 +49,13 @@ if need_part_baix:
     i = 1
     parella_i = list_dist[-i]
     peca_i = parella_i[1]
-    while (peca_i.atributs['des_product_family'] != 'Trousers' or 'Jeans' or 'Skirts' or 'Shorts' or 'Jumpsuit' or 'Leggins and joggers') and (peca_actual.atributs['des_sex'] == peca_i.atributs['des_sex']) and (peca_actual.atributs['des_age'] == peca_i.atributs['des_age']):
-        i+=1
-        parella_i = list_dist[-i]
-        peca_i = parella_i[1]
+    while ( (peca_i.atributs['des_product_family'] not in
+            ('Trousers', 'Jeans', 'Skirts', 'Shorts', 'Jumpsuit', 'Leggins and joggers'))
+            and (peca_actual.atributs['des_sex'] == peca_i.atributs['des_sex'])
+            and (peca_actual.atributs['des_age'] == peca_i.atributs['des_age']) ):
+                i+=1
+                parella_i = list_dist[-i]
+                peca_i = parella_i[1]
     outfit_final.append(peca_i)
     list_dist.pop(-i)
 
@@ -60,10 +63,13 @@ if need_part_dalt:
     i = 1
     parella_i = list_dist[-i]
     peca_i = parella_i[1]
-    while (peca_i.atributs['des_product_family'] != 'Dresses' or 'Shirt' or 'Swetter' or 'Tops' or 'T-shirt' or 'Bodysuits') and (peca_actual.atributs['des_sex'] == peca_i.atributs['des_sex']) and (peca_actual.atributs['des_age'] == peca_i.atributs['des_age']):
-        i+=1
-        parella_i = list_dist[-i]
-        peca_i = parella_i[1]
+    while ( (peca_i.atributs['des_product_family'] not in
+           ('Dresses', 'Shirt', 'Swetter', 'Tops', 'T-shirt', 'Bodysuits'))
+            and (peca_actual.atributs['des_sex'] == peca_i.atributs['des_sex'])
+            and (peca_actual.atributs['des_age'] == peca_i.atributs['des_age']) ):
+                i+=1
+                parella_i = list_dist[-i]
+                peca_i = parella_i[1]
     outfit_final.append(peca_i)
     list_dist.pop(-i)
 
@@ -84,20 +90,25 @@ if need_sabates:
 
 #suposem que acceptem com a màxim 4 items més
 i = 1
-while i <= randint(1,4):
+rand = randint(1,4)
+while i <= rand:
     parella_i = list_dist[-i]
     peca_i = parella_i[1]
     #if (mateixa edat)(mateix gènere)(no és part de dalt, ni de baix, ni sabates)
-    if (peca_actual.atributs['des_sex'] == peca_i.atributs['des_sex']) and (peca_actual.atributs['des_age'] == peca_i.atributs['des_age']) and (peca_i.atributs['des_product_family'] != 'Dresses' or 'Shirt' or 'Swetter' or 'Tops' or 'T-shirt' or 'Bodysuits') and (peca_i.atributs['des_product_family'] != 'Trousers' or 'Jeans' or 'Skirts' or 'Shorts' or 'Jumpsuit' or 'Leggins and joggers') and (peca_i.atributs['des_product_family'] != 'Footware'):
-        repetit = False
-        for product in outfit_final:
-            if peca_i.atributs['des_product_family']==product.atributs['des_product_family']:
-                repetit = True
-        if not repetit:
-            outfit_final.append(peca_i)
-            i+=1
-        else:
-            list_dist.pop(-i)
+    if ( (peca_actual.atributs['des_sex'] == peca_i.atributs['des_sex'])
+        and (peca_actual.atributs['des_age'] == peca_i.atributs['des_age'])
+        and (peca_i.atributs['des_product_family'] not in ('Dresses', 'Shirt', 'Swetter', 'Tops', 'T-shirt', 'Bodysuits'))
+        and (peca_i.atributs['des_product_family'] not in ('Trousers', 'Jeans', 'Skirts', 'Shorts', 'Jumpsuit', 'Leggins and joggers'))
+        and (peca_i.atributs['des_product_family'] != 'Footware') ):
+            repetit = False
+            for product in outfit_final:
+                if peca_i.atributs['des_product_family']==product.atributs['des_product_family']:
+                    repetit = True
+            if not repetit:
+                outfit_final.append(peca_i)
+                i+=1
+            else:
+                list_dist.pop(-i)
     else:
         list_dist.pop(-i)
 
